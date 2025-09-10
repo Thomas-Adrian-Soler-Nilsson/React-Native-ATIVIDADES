603f820ae8872340920e5596f445030c88514520
Thomas Adrian <solerthomasadrian@gmail.com>
2025-09-10 14:18:56 -0300

    Samuel Contribuição

--- appAtividade/App.js
+++ appAtividade/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, TouchableOpacity } from 'react-native';
  
 export default function App() {
   const [imagem, setImagem] = useState(require('./src/1.png'));
   const [imagem2, setImagem2] = useState(require('./src/3.png')); 
-  const [imagem3, setImagem3] = useState(require('./src/5.png')); 
+  const [imagem3, setImagem3] = useState(require('./src/5.png'));   
+  const [imagem4, setImagem4] = useState(require('./src/7.jpg')); 
+
+
+  function mudarImagem4() {
+    if (imagem4 === require('./src/7.jpg')) {
+      setImagem4(require('./src/8.jpg'));
+    } else {
+      setImagem4(require('./src/7.jpg'));
+    }
+  }
 
   function mudarImagem3() {
     if (imagem3 === require('./src/5.png')) {
@@ -37,15 +47,17 @@ export default function App() {
         <View style={styles.caixa1}>
           <TextInput placeholder='Digite seu nome' />
         </View>
-        <Button title='Mudar Imagem' onPress={mudarImagem} />
-        <Button title='Mudar Imagem 3' onPress={mudarImagem2} />
-        <Button title='Mudar Imagem 5' onPress={mudarImagem3} />
+        <TouchableOpacity style={styles.button} onPress={mudarImagem}><text> Mudar Imagem 1 </text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={mudarImagem2}><text> Mudar Imagem 3 </text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={mudarImagem3}><text> Mudar Imagem 5 </text></TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={mudarImagem4}><text> Mudar Imagem 7 </text></TouchableOpacity>
       </View>
  
       <View style={styles.container2}>
         <Image source={imagem} style={styles.img} />
         <Image source={imagem2} style={styles.img} />
         <Image source={imagem3} style={styles.img} />
+        <Image source={imagem4} style={styles.img} />
 
       </View>
     </View>
@@ -57,7 +69,7 @@ const styles = StyleSheet.create({
     flex: 1,
  },
   container1: {
-    flex: 1,
+    relative: 1,
     backgroundColor: '#f00',
     alignItems: 'center',
     justifyContent: 'center',
@@ -90,5 +102,14 @@ const styles = StyleSheet.create({
     height: 100,
     margin: 10,
     borderRadius: 50,
+  },
+  button: {
+    alignItems: 'center',
+    color: 'white',
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 10,
+    fontFamily: 'Roboto',
+    marginBottom: 10, 
   },
 });
